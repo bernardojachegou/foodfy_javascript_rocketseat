325f7465cf26b5b265d9a3973a9c234a57778852
Michel Bernardo <bernardoenglish@outlook.com>
2020-06-27 06:22:55 -0400

    recriando função para botão mostrar/esconder

--- public/scripts.js
+++ public/scripts.js
@@ -1,9 +1,4 @@
 const cards = document.querySelectorAll(".card");
-const button = document.querySelector(".button");
-const button1 = document.querySelector(".button1");
-const button2 = document.querySelector(".button2");
-
-// const buttons = document.querySelectorAll(".button");
 
 for (let card of cards) {
     card.addEventListener("click", () => {
@@ -12,44 +7,14 @@ for (let card of cards) {
     });
 }
 
-button.addEventListener("click", () => {
-    if (button.innerHTML == "MOSTRAR") {
-        button.innerHTML = "ESCONDER";
-        document.querySelector(".list").classList.remove("hide");
-    } else {
-        button.innerHTML = "MOSTRAR";
-        document.querySelector(".list").classList.add("hide");
-    }
-});
+function handleRecipeDetail(classSelected, element) {
+    document.querySelector(classSelected).classList.toggle('hide')
 
-button1.addEventListener("click", () => {
-    if (button1.innerHTML == "MOSTRAR") {
-        button1.innerHTML = "ESCONDER";
-        document.querySelector(".list1").classList.remove("hide");
+    if (element.textContent == 'ESCONDER') {
+        element.innerHTML = 'MOSTRAR'
     } else {
-        button1.innerHTML = "MOSTRAR";
-        document.querySelector(".list1").classList.add("hide");
+        element.innerHTML = 'ESCONDER'
     }
-});
+}
 
-button2.addEventListener("click", () => {
-    if (button2.innerHTML == "MOSTRAR") {
-        button2.innerHTML = "ESCONDER";
-        document.querySelector(".list2").classList.remove("hide");
-    } else {
-        button2.innerHTML = "MOSTRAR";
-        document.querySelector(".list2").classList.add("hide");
-    }
-});
 
-// for (let button of buttons) {
-//     button.addEventListener("click", () => {
-//         if (button.innerHTML == "MOSTRAR") {
-//             button.innerHTML = "ESCONDER";
-//             document.querySelector(".list").classList.remove("hide");
-//         } else {
-//             button.innerHTML = "MOSTRAR";
-//             document.querySelector(".list").classList.add("hide");
-//         }
-//     });
-// }
